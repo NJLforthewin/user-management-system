1a3140c73fe37a19db3256761ed3dee9784bc541
josephus15 <josephuserl15@gmail.com>
2025-05-14 23:29:55 -0700

    Update layout.component.ts

--- src/app/account/layout.component.ts
+++ src/app/account/layout.component.ts
@@ -1,22 +1,4 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { Router } from '@angular/router';
-
-import { AccountService } from '@app/_services';
-
-@Component({ templateUrl: 'layout.component.html' })
-export class LayoutComponent {
-    constructor(
-        private router: Router,
-        private accountService: AccountService
-    ) {
-        // redirect to home if already logged in
-        if (this.accountService.accountValue) {
-            this.router.navigate(['/']);
-        }
-    }
-}
-=======
 import { RouterModule } from '@angular/router';
 
 @Component({
@@ -25,4 +7,3 @@ import { RouterModule } from '@angular/router';
     imports: [RouterModule]
 })
 export class LayoutComponent {}
->>>>>>> 650f93b9451382bfbc24bef93c62680c8442fe72
